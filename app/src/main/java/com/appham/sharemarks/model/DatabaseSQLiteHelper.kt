c060096fce0cddab5584c06dac676e4204d6d6e5
Thomas Fuchs-Martin <t.fuchsmartin@gmail.com>
2017-11-23 10:39:45 +0700

    added a default share mark item that leads to the project's github page

--- app/src/main/java/com/appham/sharemarks/model/DatabaseSQLiteHelper.kt
+++ app/src/main/java/com/appham/sharemarks/model/DatabaseSQLiteHelper.kt
@@ -6,11 +6,12 @@ import android.database.sqlite.SQLiteOpenHelper
 import com.jskierbi.cupboard.configureCupboard
 import com.jskierbi.cupboard.cupboard
 import com.jskierbi.cupboard.register
+import java.net.URL
 
 /**
  * @author thomas
  */
-class DatabaseSQLiteHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+class DatabaseSQLiteHelper(val context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
 
     companion object {
         private val DATABASE_NAME = "marks.db"
@@ -25,6 +26,15 @@ class DatabaseSQLiteHelper(context: Context) : SQLiteOpenHelper(context, DATABAS
 
     override fun onCreate(db: SQLiteDatabase) {
         cupboard(db).createTables()
+
+        // add default item
+        cupboard(db).put(MarkItem.create(title = "GitHub - appham/Sharemarks:  Visit the open source project on github.com!",
+                content = "Sharemarks - A simple and minimalist bookmark manager app for Android. " +
+                        "Bookmarks can be added via the Android share intents. Developed with Kotlin.",
+                referrer = context.packageName,
+                url = URL("https://github.com/appham/Sharemarks"),
+                imageUrl = URL("https://github.com/fluidicon.png")))
+
     }
 
     override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
