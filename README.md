6e29cbb2b10e88ddc41a78e82a6798587adce1ee
Thomas Fuchs-Martin <t.fuchsmartin@gmail.com>
2017-11-24 19:43:05 +0700

    added more tutorial text

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ A simple and minimalist bookmark manager app for Android. Bookmarks can be added
 So far this only a prototype. The idea is that the sharemark app can save anything as a bookmark that gets shared to it via Android share intents.
 
 Usage: 
-1. In an external app that has an Android share feature (like for example Chrome Browser, Google Play Store, Facebook etc.) click the share button.
-2. In the chooser select "Sharemarks" to share the bookmark with this app
-3. Shared bookmark appears in the list. On click the shared bookmark link will be opened. 
+1. In an external app that has an Android share feature (like for example Chrome Browser, Google Play Store etc.) click the share button.
+2. In the chooser select "Sharemarks" to share the bookmark with this app.
+3. Shared bookmark appears in the list. On click the shared bookmark link will be opened.
+4. Swipe left to delete the bookmark. Swipe right to share it.
--- app/src/main/java/com/appham/sharemarks/model/DatabaseSQLiteHelper.kt
+++ app/src/main/java/com/appham/sharemarks/model/DatabaseSQLiteHelper.kt
@@ -27,13 +27,23 @@ class DatabaseSQLiteHelper(val context: Context) : SQLiteOpenHelper(context, DAT
     override fun onCreate(db: SQLiteDatabase) {
         cupboard(db).createTables()
 
-        // add default item
+        // add default items
         cupboard(db).put(MarkItem.create(title = "GitHub - appham/Sharemarks:  Visit the open source project on github.com!",
                 content = "Sharemarks - A simple and minimalist bookmark manager app for Android. " +
                         "Bookmarks can be added via the Android share intents. Developed with Kotlin.",
                 referrer = context.packageName,
                 url = URL("https://github.com/appham/Sharemarks"),
                 imageUrl = URL("https://github.com/fluidicon.png")))
+
+        cupboard(db).put(MarkItem.create(title = "How to use Sharemarks: ",
+                content = "1. In an external app that has an Android share feature " +
+                        "(like for example Chrome Browser, Google Play Store etc.) click the share button. \n" +
+                        "2. In the chooser select \"Sharemarks\" to share the bookmark with this app. \n" +
+                        "3. Shared bookmark appears in the list. On click the shared bookmark link will be opened. \n" +
+                        "4. Swipe left to delete the bookmark. Swipe right to share it.",
+                referrer = context.packageName,
+                url = URL("https://github.com/appham/Sharemarks/blob/master/README.md"),
+                imageUrl = null))
 
     }
 
