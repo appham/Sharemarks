3d727899d8e56b978717911527fbe3e900b02540
Thomas Fuchs-Martin <t.fuchsmartin@gmail.com>
2017-11-25 10:12:25 +0700

    remove unused method

--- app/src/main/java/com/appham/sharemarks/utils/Utils.kt
+++ app/src/main/java/com/appham/sharemarks/utils/Utils.kt
@@ -2,7 +2,6 @@ package com.appham.sharemarks.utils
 
 import android.content.Context
 import android.content.res.Configuration
-import android.content.res.Resources
 import android.os.Build
 import android.util.Log
 import android.util.Patterns
@@ -86,8 +85,6 @@ class Utils {
         fun isTablet(context: Context): Boolean {
             return context.resources.configuration.screenLayout and Configuration.SCREENLAYOUT_SIZE_MASK >= Configuration.SCREENLAYOUT_SIZE_LARGE
         }
-
-        fun getScreenWidth(): Int = Resources.getSystem().displayMetrics.widthPixels
 
     }
 }
