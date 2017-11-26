13bdbe58a922ac8452afeed210fc60e3070aeb48
Thomas Fuchs-Martin <t.fuchsmartin@gmail.com>
2017-11-26 20:33:26 +0700

    set default picasso instance with timeouts and error logger

--- app/src/main/java/com/appham/sharemarks/view/MarksApplication.kt
+++ app/src/main/java/com/appham/sharemarks/view/MarksApplication.kt
@@ -1,8 +1,31 @@
 package com.appham.sharemarks.view
 
 import android.app.Application
+import android.util.Log
+import com.squareup.okhttp.OkHttpClient
+import com.squareup.picasso.OkHttpDownloader
+import com.squareup.picasso.Picasso
+import java.util.concurrent.TimeUnit
 
 /**
  * @author thomas
  */
-class MarksApplication : Application()
+class MarksApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        //Setup Picasso instance with timeouts
+        val httpClient = OkHttpClient()
+        httpClient.setConnectTimeout(10, TimeUnit.SECONDS)
+        httpClient.setReadTimeout(10, TimeUnit.SECONDS)
+        val picasso = Picasso.Builder(applicationContext)
+                .downloader(OkHttpDownloader(httpClient))
+                .listener { _, uri, e ->
+                    e.printStackTrace()
+                    Log.e(Picasso::class.simpleName, e.message?.plus(" at uri ").plus(uri))
+                }
+                .build()
+        Picasso.setSingletonInstance(picasso)
+
+    }
+}
