310c115d5a820be900abc321cc061aeab7af2e5a
Thomas Fuchs-Martin <t.fuchsmartin@gmail.com>
2017-11-27 17:46:20 +0700

    add test rule

--- app/src/test/java/com/appham/sharemarks/HtmlManagerTest.kt
+++ app/src/test/java/com/appham/sharemarks/HtmlManagerTest.kt
@@ -2,11 +2,8 @@ package com.appham.sharemarks
 
 import com.appham.sharemarks.model.MarkItem
 import com.appham.sharemarks.presenter.HtmlManager
-import org.junit.AfterClass
+import org.junit.*
 import org.junit.Assume.assumeTrue
-import org.junit.Before
-import org.junit.BeforeClass
-import org.junit.Test
 import java.io.File
 import java.io.InputStream
 import java.net.URL
@@ -54,6 +51,9 @@ class HtmlManagerTest {
             proc.waitFor(5, TimeUnit.SECONDS)
         }
 
+        @get:Rule
+        val htmlManager = HtmlManager()
+
     }
 
     @Before
@@ -74,7 +74,6 @@ class HtmlManagerTest {
     (expected = RuntimeException::class)
     fun `not found page should lead to exception`() {
         val url = URL(testPath.plus("notfoundpage404"))
-        val htmlManager = HtmlManager()
         val observable = htmlManager.parseHtml(url, MarkItem.create(null, null, null, url, null))
         observable.blockingFirst()
     }
@@ -82,7 +81,6 @@ class HtmlManagerTest {
     @Test
     fun `on pages without images the image url should be favicon url`() {
         val url = URL(testPath.plus("test-no-img.html"))
-        val htmlManager = HtmlManager()
         val observable = htmlManager.parseHtml(url, MarkItem.create(null, null, null, url, null))
         val item = observable.blockingFirst()
         assertEquals("http://localhost/favicon.ico", item.imageUrl)
