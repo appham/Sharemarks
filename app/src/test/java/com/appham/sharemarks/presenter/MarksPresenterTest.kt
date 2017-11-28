1d815dd69ca1f232878e916dbc94ead626dc552a
Thomas Fuchs-Martin <t.fuchsmartin@gmail.com>
2017-11-28 22:04:53 +0700

    add more MarksPresenter tests

--- app/src/test/java/com/appham/sharemarks/presenter/MarksPresenterTest.kt
+++ app/src/test/java/com/appham/sharemarks/presenter/MarksPresenterTest.kt
@@ -2,9 +2,7 @@ package com.appham.sharemarks.presenter
 
 import android.content.Intent
 import com.appham.sharemarks.model.MarksDataSource
-import com.nhaarman.mockito_kotlin.mock
-import com.nhaarman.mockito_kotlin.spy
-import com.nhaarman.mockito_kotlin.verify
+import com.nhaarman.mockito_kotlin.*
 import org.junit.Test
 
 /**
@@ -17,15 +15,27 @@ class MarksPresenterTest {
     val presenter: MarksPresenter = spy(MarksPresenter(view, dataSource, mutableListOf()))
 
     @Test
-    fun `Verify handleSendText method is called with the shared text and referrer`() {
+    fun `handleSendText with shared url in text is called correctly`() {
         presenter.handleSharedData(Intent.ACTION_SEND, "text/plain", "http://appham.com", "x")
         verify(presenter).handleSendText("http://appham.com", "x")
     }
 
     @Test
-    fun `Verify handleSendText method is called correctly if no url shared`() {
+    fun `handleSendText with no shared url is called correctly`() {
         presenter.handleSharedData(Intent.ACTION_SEND, "text/plain", "no url here..", "x")
         verify(presenter).handleSendText("no url here..", "x")
+    }
+
+    @Test
+    fun `handleSendText with empty text and null referrer is called correctly`() {
+        presenter.handleSharedData(Intent.ACTION_SEND, "text/plain", "", null)
+        verify(presenter).handleSendText("", null)
+    }
+
+    @Test
+    fun `handleSendText with for image type action should not be called`() {
+        presenter.handleSharedData(Intent.ACTION_SEND, "image/", "", "")
+        verify(presenter, never()).handleSendText(anyOrNull(), anyOrNull())
     }
 
 }
