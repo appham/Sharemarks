6a2585da7fce84487cb5caa59dea66d80d99026b
Thomas Fuchs-Martin <t.fuchsmartin@gmail.com>
2017-11-29 00:24:40 +0700

    first simple espresso test added

---
+++ app/src/androidTest/java/com/appham/sharemarks/MarksActivityTest.kt
@@ -0,0 +1,41 @@
+package com.appham.sharemarks
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.Espresso.pressBack
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
+import android.support.test.espresso.matcher.ViewMatchers.withId
+import android.support.test.rule.ActivityTestRule
+import android.support.test.runner.AndroidJUnit4
+import com.appham.sharemarks.view.MarksActivity
+import org.junit.Assert.assertEquals
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class MarksActivityTest {
+
+    @get:Rule
+    val activityRule = ActivityTestRule<MarksActivity>(MarksActivity::class.java)
+
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("com.appham.sharemarks", appContext.packageName)
+    }
+
+    @Test
+    fun onBackPressOpenFilters() {
+        pressBack()
+        onView(withId(R.id.navView)).check(matches(isDisplayed()))
+    }
+
+}
